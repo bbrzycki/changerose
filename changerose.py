79fac46785d31d3ae80fb034540be189c5cdcb1d
Bryan Brzycki <bbrzycki@college.harvard.edu>
2018-03-21 16:55:53 -0400

    Add 'blend' functionality, as well as argument parsing

--- changerose.py
+++ changerose.py
@@ -1,28 +1,208 @@
 from PIL import Image
 import glob, os, sys, errno
 import numpy as np
+import argparse
 
 BOUNDARY_WIDTH = 1
 
-def copy_image(argv):
+# return a tuple of total size and paste position
+def get_new_size(image):
+    # Set requisite dimensions. Center original image within new image.
+    max_dimension = max(image.width, image.height)
+    return (max_dimension, max_dimension)
+
+def get_paste_position(image):
+    max_dimension = max(image.width, image.height)
+    return (int(np.floor(max_dimension/2))
+                     -int(np.floor(image.width/2)),
+                      int(np.floor(max_dimension/2))
+                     -int(np.floor(image.height/2)))
+
+# colors are tuples, positions are ints
+def get_intermediate_pixel(position, edge_position, boundary_position,
+                           background_color, boundary_color):
+    return tuple([int(a + np.floor((b - a)/(boundary_position - edge_position)
+        * (position - edge_position)))
+        for (a,b) in zip(background_color,boundary_color)])
+
+def average_color(*images):
+    r_tot = 0
+    g_tot = 0
+    b_tot = 0
+    pixel_num = 0
+    for image in images:
+        rgb_data = image.getdata()
+        for (r,g,b) in rgb_data:
+            r_tot += r
+            g_tot += g
+            b_tot += b
+        pixel_num += len(rgb_data)
+    return (int(np.round(r_tot / pixel_num)),
+            int(np.round(g_tot / pixel_num)),
+            int(np.round(b_tot / pixel_num)))
+
+# def average_color(image1, image2):
+#     rgb_data1 = image1.getdata()
+#     rgb_data2 = image2.getdata()
+#     r_tot = 0
+#     g_tot = 0
+#     b_tot = 0
+#     for (r,g,b) in rgb_data1:
+#         r_tot += r
+#         g_tot += g
+#         b_tot += b
+#     for (r,g,b) in rgb_data2:
+#         r_tot += r
+#         g_tot += g
+#         b_tot += b
+#     pixel_num = len(rgb_data1) + len(rgb_data2)
+#     return (int(np.round(r_tot / pixel_num)),
+#             int(np.round(g_tot / pixel_num)),
+#             int(np.round(b_tot / pixel_num)))
+
+def average_first_boundary_color(image, boundary_width = BOUNDARY_WIDTH):
+    # Analyze border of image to calculate mean color
+    if image.width > image.height:
+        top_boundary = image.crop((0,
+                                    0,
+                                    image.width,
+                                    boundary_width)).convert('RGB')
+        return average_color(top_boundary)
+    elif image.width < image.height:
+        left_boundary = image.crop((0,
+                                     0,
+                                     boundary_width,
+                                     image.height)).convert('RGB')
+        return average_color(left_boundary)
+    else:
+        # Image is already square to begin with, background color doesn't matter
+        return (0, 0, 0)
+
+def average_second_boundary_color(image, boundary_width = BOUNDARY_WIDTH):
+    # Analyze border of image to calculate mean color
+    if image.width > image.height:
+        bot_boundary = image.crop((0,
+                                    image.height-boundary_width,
+                                    image.width,
+                                    image.height)).convert('RGB')
+        return average_color(bot_boundary)
+    elif image.width < image.height:
+        right_boundary = image.crop((image.width-boundary_width,
+                                      0,
+                                      image.width,
+                                      image.height)).convert('RGB')
+        return average_color(right_boundary)
+    else:
+        # Image is already square to begin with, background color doesn't matter
+        return (0, 0, 0)
+
+def average_boundary_color(image, boundary_width = BOUNDARY_WIDTH):
+    # Analyze border of image to calculate mean color
+    if image.width > image.height:
+        top_boundary = image.crop((0,
+                                    0,
+                                    image.width,
+                                    boundary_width)).convert('RGB')
+        bot_boundary = image.crop((0,
+                                    image.height-boundary_width,
+                                    image.width,
+                                    image.height)).convert('RGB')
+        return average_color(top_boundary, bot_boundary)
+    elif image.width < image.height:
+        left_boundary = image.crop((0,
+                                     0,
+                                     boundary_width,
+                                     image.height)).convert('RGB')
+        right_boundary = image.crop((image.width-boundary_width,
+                                      0,
+                                      image.width,
+                                      image.height)).convert('RGB')
+        return average_color(left_boundary, right_boundary)
+    else:
+        # Image is already square to begin with, background color doesn't matter
+        return (0, 0, 0)
+
+def color_background(image, color = 0, boundary_width = BOUNDARY_WIDTH):
+    return Image.new('RGB',get_new_size(image),color)
+
+def average_background(image, boundary_width = BOUNDARY_WIDTH):
+    background_color = average_boundary_color(image, boundary_width)
+    return Image.new('RGB',get_new_size(image),background_color)
+
+def blend_background(image):
+    background = average_background(image, boundary_width = 1)
+    first_background_color = average_first_boundary_color(image, boundary_width = 1)
+    second_background_color = average_second_boundary_color(image, boundary_width = 1)
+    # Analyze border of image to calculate mean color
+    if image.width > image.height:
+        top_boundary_y = get_paste_position(image)[1]
+        bot_boundary_y = top_boundary_y + image.height
+        for x in np.arange(0, image.width):
+            x = int(x)
+            top_boundary_color = image.getpixel((x, 0))
+            bot_boundary_color = image.getpixel((x, image.height - 1))
+            for y in np.arange(0, top_boundary_y):
+                y = int(y)
+                new_color = get_intermediate_pixel(y,
+                                                   0,
+                                                   top_boundary_y,
+                                                   first_background_color,
+                                                   top_boundary_color)
+                background.putpixel((x,y), new_color)
+
+            for y in np.arange(bot_boundary_y, image.width):
+                y = int(y)
+                new_color = get_intermediate_pixel(y,
+                                                   image.width - 1,
+                                                   bot_boundary_y - 1,
+                                                   second_background_color,
+                                                   bot_boundary_color)
+                background.putpixel((x,y), new_color)
+
+    elif image.width < image.height:
+        left_boundary_x = get_paste_position(image)[0]
+        right_boundary_x = left_boundary_x + image.width
+        for y in np.arange(0, image.height):
+            y = int(y)
+            left_boundary_color = image.getpixel((0, y))
+            right_boundary_color = image.getpixel((image.width - 1, y))
+            for x in np.arange(0, left_boundary_x):
+                x = int(x)
+                new_color = get_intermediate_pixel(x,
+                                                   0,
+                                                   left_boundary_x,
+                                                   first_background_color,
+                                                   left_boundary_color)
+                background.putpixel((x,y), new_color)
+
+            for x in np.arange(right_boundary_x, image.height):
+                x = int(x)
+                new_color = get_intermediate_pixel(x,
+                                                   image.height - 1,
+                                                   right_boundary_x - 1,
+                                                   second_background_color,
+                                                   right_boundary_color)
+                background.putpixel((x,y), new_color)
+
+    else:
+        # Image is already square
+        pass;
+
+    return background
+
+def copy_image(filenames = '*.png', method = 'color', color = 0):
     """
     Create image copies with square dimensions by adding additional space to the
     original image. Image copies may fill background with either a solid color
     (default: white) or a computed average based on the edge of the original
     image.
     """
-    # Check correct number of arguments
-    if len(argv) < 3:
-        sys.exit('Usage: python %s <filenames> <background_type>' % (argv[0],))
 
-    # input_files = glob.glob(argv[1])
-    input_files = argv[2:]
-    background_type = argv[1]
+    input_files = glob.glob(filenames)
 
     if len(input_files) == 0:
         sys.exit('The expression \'%s\' does not match any filenames!'
             % (argv[1],))
-
 
     for input_file in input_files:
 
@@ -43,85 +223,48 @@ def copy_image(argv):
         im_copy = im.copy()
         im.close()
 
-        # Set requisite dimensions. Center original image within new image.
-        max_dimension = max(im_copy.width, im_copy.height)
-        paste_position = (int(np.floor(max_dimension/2))
-                         -int(np.floor(im_copy.width/2)),
-                          int(np.floor(max_dimension/2))
-                         -int(np.floor(im_copy.height/2)))
-        new_size = (max_dimension, max_dimension)
-
-        # Set background color of image copy.
-        background_color = 0
-        if background_type == 'average':
-            r_tot = 0
-            g_tot = 0
-            b_tot = 0
-            # Analyze border of image to calculate mean color
-            if im_copy.width > im_copy.height:
-                im_top_bound = im_copy.crop((0,
-                                            0,
-                                            im_copy.width,
-                                            BOUNDARY_WIDTH)).convert('RGB')
-                top_bound = im_top_bound.getdata()
-                im_bot_bound = im_copy.crop((0,
-                                            im_copy.height-BOUNDARY_WIDTH,
-                                            im_copy.width,
-                                            im_copy.height)).convert('RGB')
-                bot_bound = im_bot_bound.getdata()
-                for (r,g,b) in top_bound:
-                    r_tot += r
-                    g_tot += g
-                    b_tot += b
-                for (r,g,b) in bot_bound:
-                    r_tot += r
-                    g_tot += g
-                    b_tot += b
-                pixel_num = len(top_bound) + len(bot_bound)
-                background_color = (int(np.round(r_tot / pixel_num)),
-                                    int(np.round(g_tot / pixel_num)),
-                                    int(np.round(b_tot / pixel_num)))
-            elif im_copy.width < im_copy.height:
-                im_left_bound = im_copy.crop((0,
-                                             0,
-                                             BOUNDARY_WIDTH,
-                                             im_copy.height)).convert('RGB')
-                left_bound = im_left_bound.getdata()
-                im_right_bound = im_copy.crop((im_copy.width-BOUNDARY_WIDTH,
-                                              0,
-                                              im_copy.width,
-                                              im_copy.height)).convert('RGB')
-                right_bound = im_right_bound.getdata()
-                for (r,g,b) in left_bound:
-                    r_tot += r
-                    g_tot += g
-                    b_tot += b
-                for (r,g,b) in right_bound:
-                    r_tot += r
-                    g_tot += g
-                    b_tot += b
-                pixel_num = len(left_bound) + len(right_bound)
-                background_color = (int(np.round(r_tot / pixel_num)),
-                                    int(np.round(g_tot / pixel_num)),
-                                    int(np.round(b_tot / pixel_num)))
-            else:
-                # Image is already square to begin with
-                pass
+        # Create background image layer
+        if method == 'color':
+            final_im = color_background(im_copy, color = color)
+        elif method == 'average':
+            final_im = average_background(im_copy)
+        elif method == 'blend':
+            final_im = blend_background(im_copy)
         else:
-            # Parse background_type argument
-            background_color = background_type
+            sys.exit('Invalid color method specified!')
 
-        # Create final image
-        background_im = Image.new('RGB',new_size,background_color)
-        background_im.paste(im_copy, paste_position)
+        final_im.paste(im_copy, get_paste_position(im_copy))
+
         im_copy.close()
 
-        # Save new image
+        # Save final image with informative filename
         root, ext = os.path.splitext(filename)
-        background_im.save(new_dirname + root + '_changerose' + ext)
-        background_im.close()
 
+        final_filename = ''
+        if method == 'color':
+            final_filename = new_dirname + root + '_changerose_' \
+                           + method + '_' + color + ext
+        else:
+            final_filename = new_dirname + root + '_changerose_' \
+                           + method + ext
 
+        final_im.save(final_filename)
+        final_im.close()
+
+def main():
+    parser = argparse.ArgumentParser(description='Make a square image ' +
+        "according to a certain background color prescription.")
+    parser.add_argument('-m', '--method', type=str, dest='method',
+                        help='method for generating background ' +
+                            '(average, blend, color)')
+    parser.add_argument('-c', '--color', dest='color', default=0,
+                        help='background color')
+    parser.add_argument('filenames', type=str,
+                        help='regular expression for filenames')
+
+    args = parser.parse_args()
+
+    copy_image(args.filenames, args.method, args.color)
 
 if __name__ == '__main__':
-    copy_image(sys.argv)
+    main()
